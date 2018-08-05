1cec9b68aa58e55ca880554828eed76993a6b48b
Ritesh <ritesh101094@gmail.com>
2018-08-05 14:37:37 +0530

    Added global script for project

--- bin/global.js
+++ bin/global.js
@@ -1,20 +1,21 @@
-const PNGExport = require("./src/png-export");
-const JsGenerator = require("./src/js-generator");
+#!/usr/bin/env node
+
+const PNGExport = require("../src/png-export");
+const JsGenerator = require("../src/js-generator");
 const path = require("path");
 
 const program = require("commander");
 
 program
-    .version('1.0.0', "-v", "--version")
-    .usage("command [options]")
-    .option("-O, --out <path>", "Output path of generated image", path.join(__dirname, "dist"))
-    .option("-S, --src <path>", "Root path of source application", path.join(__dirname, "src"))
+    .usage("[options]")
+    .option("-O, --out <path>", "Output path of generated image", process.cwd())
+    .option("-S, --src <path>", "Root path of source application", process.cwd())
     .option("-W, --width <n>", "Root path of source application", parseInt)
     .option("-H, --height <n>", "Root path of source application", parseInt)
     .action(async () => {
         program.width = program.width || 3000;
         program.height = program.height || 3000;
-        
+
         PNGExport(
             JsGenerator,
             path.join(program.src),
@@ -22,7 +23,7 @@ program
             program.width,
             program.height
         )
-        .then(() => console.log("Generated architecture"))
+        .then(() => console.log("Generated architecture at " + path.join(program.out, "out.png")))
         .catch(err => console.error("Failed: ", err));
     })
     .parse(process.argv);
